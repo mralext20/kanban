c06b01e3eb884ed16bd808d4e81c8adc475d1f48
Riley Tudbury <tudbury@gmail.com>
2020-03-02 11:57:50 -0700

    basic home layout

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="home">
-    <h1>This is your home page.</h1>
+  <div class="home text-light container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="pt-3">Welcome to your Project Boards</h1>
+        <h5
+          class="pt-5"
+        >Our project boards allow you to quickly and easily organize tasks into lists and individual items.</h5>
+        <h5>Simply login to get started and create your first board!</h5>
+      </div>
+    </div>
   </div>
 </template>
 
