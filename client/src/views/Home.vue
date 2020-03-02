232dd04e1345d44c9e39fe471c33e225a10878aa
Riley Tudbury <tudbury@gmail.com>
2020-03-02 13:47:10 -0700

    login functionality

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,7 +6,10 @@
         <h5
           class="pt-5"
         >Your project boards allow you to quickly and easily organize tasks into lists and individual items.</h5>
-        <h5>Simply login to get started and create your first board!</h5>
+        <h5 v-if="!$auth.isAuthenticated">
+          Simply
+          <span id="login-span" @click="login" v-if="!$auth.isAuthenticated">login</span> to get started and create your first board!
+        </h5>
       </div>
     </div>
   </div>
@@ -14,17 +17,39 @@
 
 
 <script>
+import axios from "axios";
+
+let _api = axios.create({
+  baseURL: "https://localhost:3000",
+  withCredentials: true
+});
+
 export default {
   name: "home",
   data() {
     return {};
   },
   computed: {},
-  methods: {},
+  methods: {
+    async login() {
+      await this.$auth.loginWithPopup();
+      this.$store.dispatch("setBearer", this.$auth.bearer);
+      this.$store.dispatch("getProfile");
+      console.log("this.$auth.user: ");
+      console.log(this.$auth.user);
+    }
+  },
   components: {}
 };
 </script>
 
 
 <style scoped>
+span {
+  text-decoration: underline;
+}
+#login-span:hover {
+  color: lightblue;
+  cursor: pointer;
+}
 </style>
