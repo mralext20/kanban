98f2e27f9427337256d45fd69ca8102a16b5d22c
Riley Tudbury <tudbury@gmail.com>
2020-03-02 14:56:31 -0700

    board and boards styling

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="boards">
-    <h2>Your Boards</h2>
-    <form @submit.prevent="addBoard">
-      <input type="text" placeholder="title" v-model="newBoard.title" required />
-      <input type="text" placeholder="description" v-model="newBoard.description" />
-      <button type="submit">Create Board</button>
-    </form>
-    <h3
-      class="pt-5"
-      v-if="!boards"
-    >Looks pretty empty here huh? Once you start adding some boards they will appear below!</h3>
-    <div v-for="board in boards" :key="board._id">
-      <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
+  <div class="boards container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h2>Your Boards</h2>
+        <form @submit.prevent="addBoard">
+          <input type="text" placeholder="title" v-model="newBoard.title" required />
+          <input type="text" placeholder="description" v-model="newBoard.description" />
+          <button type="submit">Create Board</button>
+        </form>
+        <h3
+          class="pt-5"
+          v-if="!boards"
+        >Looks pretty empty here huh? Once you start adding some boards they will appear below!</h3>
+      </div>
+      <div class="col-12 text-center pt-3">
+        <div class="card mx-auto" style="width: 18rem;">
+          <div class="card-header text-dark">Your Boards</div>
+
+          <ul class="list-group list-group-flush">
+            <div v-for="board in boards" :key="board._id">
+              <li class="list-group-item">
+                <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -43,3 +57,6 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+</style>
