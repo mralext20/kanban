437761129518bab37e082d91f3c0cf468e3915a4
Alexander Terry <alexterry43@gmail.com>
2020-03-03 11:35:29 -0700

    populate boards on edit and create

--- server/services/BoardService.js
+++ server/services/BoardService.js
@@ -8,14 +8,15 @@ class BoardService {
       .populate("creator", "name picture")
       .populate("lists")
       .populate({ path: "lists", populate: { path: "tasks" } })
-    // .populate({ path: "lists.tasks", populate: { path: "comments" } })
-
 
     return board;
   }
 
   async getById(id, userEmail) {
-    let data = await dbContext.Boards.findOne({ _id: id, creatorEmail: userEmail });
+    let data = await dbContext.Boards.findOne({ _id: id, creatorEmail: userEmail })
+      .populate("creator", "name picture")
+      .populate("lists")
+      .populate({ path: "lists", populate: { path: "tasks" } });
     if (!data) {
       throw new BadRequest("Invalid ID or you do not own this board");
     }
@@ -24,11 +25,19 @@ class BoardService {
 
   async create(rawData) {
     let data = await dbContext.Boards.create(rawData);
+    await data.populate("creator", "name picture")
+      .populate("lists")
+      .populate({ path: "lists", populate: { path: "tasks" } })
+      .execPopulate()
     return data;
   }
 
   async edit(id, userEmail, update) {
     let data = await dbContext.Boards.findOneAndUpdate({ _id: id, creatorEmail: userEmail }, update, { new: true });
+    await data.populate("creator", "name picture")
+      .populate("lists")
+      .populate({ path: "lists", populate: { path: "tasks" } })
+      .execPopulate();
     if (!data) {
       throw new BadRequest("Invalid ID or you do not own this board");
     }
