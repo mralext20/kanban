e008bbf6107f9ef5c4f73e51388cc2da8a3078fc
Alexander Terry <alexterry43@gmail.com>
2020-03-03 16:16:11 -0700

    I CAN MOVE TASKS

--- client/src/components/TaskComponent.vue
+++ client/src/components/TaskComponent.vue
@@ -1,5 +1,26 @@
 <template>
-  <li>{{taskData.body}}</li>
+  <li>
+    {{taskData.body}}
+    <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >Move To..</button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <button
+          v-for="list in board.lists"
+          :key="list.id"
+          v-show="list.id != taskData.listId"
+          class="dropdown-item"
+          @click="moveTo(list.id)"
+        >{{list.title}}</button>
+      </div>
+    </div>
+  </li>
 </template>
 
 <script>
@@ -14,6 +35,17 @@ export default {
   methods: {
     deleteTask() {
       this.$store.dispatch("deleteTask", this.taskData.id);
+    },
+    moveTo(listId) {
+      this.$store.dispatch("moveTasks", {
+        task: this.taskData,
+        target: listId
+      });
+    }
+  },
+  computed: {
+    board() {
+      return this.$store.state.activeBoard;
     }
   }
 };
